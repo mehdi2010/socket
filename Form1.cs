4b7149726766fa0fd5ed7350bb6adb31d68ee3db
mehdi <mehdi2010.20@gmail.com>
2023-04-05 00:02:53 +0430

    first commit

---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+using System.Threading;
+
+namespace socket
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static TcpListener tcpListener = new TcpListener(10);
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tcpListener.Start();
+            Console.WriteLine("************This is Server program************");
+            Console.WriteLine("Hoe many clients are going to connect to this server?:");
+            int numberOfClientsYouNeedToConnect = int.Parse(textBox1.Text);
+            for (int i = 0; i < numberOfClientsYouNeedToConnect; i++)
+            {
+                Thread newThread = new Thread(new ThreadStart(Listeners));
+                newThread.Start();
+            }
+
+
+        }
+        static void Listeners()
+        {
+
+            Socket socketForClient = tcpListener.AcceptSocket();
+            if (socketForClient.Connected)
+            {
+                Console.WriteLine("Client:" + socketForClient.RemoteEndPoint + " now connected to server.");
+                NetworkStream networkStream = new NetworkStream(socketForClient);
+                System.IO.StreamWriter streamWriter =
+                new System.IO.StreamWriter(networkStream);
+                System.IO.StreamReader streamReader =
+                new System.IO.StreamReader(networkStream);
+
+                ////here we send message to client
+                //Console.WriteLine("type your message to be recieved by client:");
+                //string theString = Console.ReadLine();
+                //streamWriter.WriteLine(theString);
+                ////Console.WriteLine(theString);
+                //streamWriter.Flush();
+
+                //while (true)
+                //{
+                //here we recieve client's text if any.
+                while (true)
+                {
+                    string theString = streamReader.ReadLine();
+                    Console.WriteLine("Message recieved by client:" + theString);
+                    if (theString == "exit")
+                        break;
+                }
+                streamReader.Close();
+                networkStream.Close();
+                streamWriter.Close();
+                //}
+
+            }
+            socketForClient.Close();
+            Console.WriteLine("Press any key to exit from server program");
+            Console.ReadKey();
+        }
+
+
+    }
+}
